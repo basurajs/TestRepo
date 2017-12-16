6eba753696aacb46e1753a492ee30a3133103fcb
Basavaraj Lamani <balamani@cisco.com>
2017-12-16 07:32:49 +0530

    added test1.py file

---
+++ test1.py
@@ -0,0 +1,7 @@
+text = input("enter the name:")
+if text == "basu":
+    print("He is Software engineer")
+elif text =="bharati":
+    print("she is house wife")
+else:
+    print("we don't know him")
